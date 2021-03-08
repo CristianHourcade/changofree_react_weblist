5b8d2fac11dbc5010e9dc59b6e339570fe18e135
CristianHourcade [goiar] <hourcadecristian@gmail.com>
2021-03-08 13:19:58 -0300

    generador de sitemap

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,60 @@
 import React from "react";
 import { getFirebaseRB } from "../firebase";
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
 const RouterInitial = ({ domains }) => {
-  console.log(domains);
+  const download = () => {
+    document.open(
+      "data:Application/octet-stream," + encodeURIComponent(domains)
+    );
+  };
   return (
     <>
-      {domains.map((element, index) => {
-        return <div key={index} ><a href={`https://${element}`}>{element}</a></div>;
-      })}
+      <a href="#" onClick={download} download="file.xml">Descargar XML</a>
+      {domains}
     </>
   );
 };
 
 export default RouterInitial;
 export const getServerSideProps = async ({ req, res }) => {
+  const stream = new SitemapStream();
   let domains = [];
   await getFirebaseRB()
     .ref("usuarios")
     .once("value", (value) => {
       Object.keys(value.val()).map((key) => {
         let element = value.val()[key];
-        console.log(element)
+        console.log(element);
         if (element.dominio) {
-          domains.push(element.dominio.url);
+          domains.push({
+            url: "https://" + element.dominio.url,
+            changefreq: "daily",
+            priority: 0.3,
+          });
         } else {
-          domains.push(`${element.marca}.changofree.com`);
+          domains.push({
+            url: `https://${element.marca}.changofree.com`,
+            changefreq: "daily",
+            priority: 0.3,
+          });
+        }
+        if (element.productos) {
+          Object.keys(element.productos).map((key) => {
+            domains.push({
+              url: `https://${(element.dominio) ? element.dominio.url : element.marca+'.changofree.com'}/${element.marca}/producto/${element.productos[key].name}`,
+              changefreq: "daily",
+              priority: 0.3,
+            });
+          });
         }
       });
     });
-  return { props: { domains: domains } };
+  return {
+    props: {
+      domains: await (
+        await streamToPromise(Readable.from(domains).pipe(stream))
+      ).toString(),
+    },
+  };
 };
